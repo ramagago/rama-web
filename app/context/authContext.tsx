1a8d12114ea142175632c1c8780ddb5fb86d958e
Rama <rama@Ramas-MacBook-Pro.local>
2024-08-18 15:37:00 -0300

    setAdmin True

--- app/context/authContext.tsx
+++ app/context/authContext.tsx
@@ -19,7 +19,7 @@ interface AuthProviderProps {
 }
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(true)
   const [editMode, setEditMode] = useState(false)
   const [isPhotoDetail, setIsPhotoDetail] = useState(false)
 
